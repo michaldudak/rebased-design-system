5ddb36a67bf6dd9532f484fed24ca1f9028c1f8b
Michał Dudak <michal@dudak.me>
2022-09-21 14:35:19 +0200

    Create the Button component

---
+++ src/Button/Button.tsx
@@ -0,0 +1,8 @@
+import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+import styled from '@emotion/styled';
+
+const Button = styled(ButtonUnstyled)({
+	backgroundColor: 'blue',
+});
+
+export { Button };
