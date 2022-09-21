c0848a742f7ff9f92df2d75fd0a16ab0d3e0a235
Michał Dudak <michal@dudak.me>
2022-09-21 16:15:13 +0200

    Add Button size prop

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -6,41 +6,60 @@ export enum ButtonVariant {
 	outlined = 'outlined',
 }
 
+export enum ButtonSize {
+	small = 'small',
+	medium = 'medium',
+	large = 'large',
+}
+
 export interface ButtonProps extends ButtonUnstyledProps {
+	size?: ButtonSize;
 	variant?: ButtonVariant;
 }
 
 export const Button = styled(ButtonUnstyled)(
-	({ variant = ButtonVariant.filled }: ButtonProps) => `
-	--button-color: #116466;
-	background-color: ${variant === ButtonVariant.filled ? 'var(--button-color)' : 'transparent'};
-	font-family: 'Kanit', sans-serif;
-	font-size: 0.875rem;
-	padding: ${variant === ButtonVariant.filled ? '8px 16px' : '6px 14px'};
-	border-radius: 2px;
-	border: ${variant === ButtonVariant.filled ? 'none' : '2px solid var(--button-color)'};
-	outline-offset: 0;
-	outline: 0 solid var(--button-color);
-	transition: outline-width 0.1s ease-in, outline-offset 0.1s ease-in;
-	color: ${variant === ButtonVariant.filled ? '#fff' : 'var(--button-color)'};
+	({ variant = ButtonVariant.filled, size = ButtonSize.medium }: ButtonProps) => {
+		let padding: string;
+		let fontSize = '0.875rem';
+		if (size === ButtonSize.small) {
+			padding = '2px 6px';
+		} else if (size === ButtonSize.medium) {
+			padding = '6px 14px';
+		} else {
+			padding = '10px 22px';
+			fontSize = '1rem';
+		}
+		return `
+			--button-color: #116466;
+			background-color: ${variant === ButtonVariant.filled ? 'var(--button-color)' : 'transparent'};
+			font-family: 'Kanit', sans-serif;
+			font-size: ${fontSize};
+			padding: ${padding};
+			border-radius: 2px;
+			border: 2px solid var(--button-color);
+			outline-offset: 0;
+			outline: 0 solid var(--button-color);
+			transition: outline-width 0.1s ease-in, outline-offset 0.1s ease-in;
+			color: ${variant === ButtonVariant.filled ? '#fff' : 'var(--button-color)'};
 
-	&:hover:where(:not(.${buttonUnstyledClasses.disabled})) {
-		outline-width: 1px;
-		outline-offset: 2px;
-	}
+			&:hover:where(:not(.${buttonUnstyledClasses.disabled})) {
+				outline-width: 1px;
+				outline-offset: 2px;
+			}
 
-	&:focus-visible {
-		outline-width: 2px;
-		outline-offset: 2px;
-	}
+			&:focus-visible {
+				outline-width: 2px;
+				outline-offset: 2px;
+			}
 
-	&.${buttonUnstyledClasses.disabled} {
-		opacity: 0.5;
-	}
+			&.${buttonUnstyledClasses.disabled} {
+				opacity: 0.5;
+			}
 
-	&.${buttonUnstyledClasses.active} {
-		outline-offset: 4px;
-		transition: outline-offset 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+			&.${buttonUnstyledClasses.active} {
+				outline-offset: 4px;
+				transition: outline-offset 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+			}
+		`;
 	}
-`
 ) as React.FC<ButtonProps>;
