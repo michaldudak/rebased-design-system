e44bcbd79d44833720941f964cc2a6479beb5a45
Michał Dudak <michal@dudak.me>
2023-10-11 16:10:26 +0200

    Allow the package to be consumable

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,17 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), dts()],
+	plugins: [
+		react(),
+		dts({
+			tsconfigPath: './tsconfig.build.json',
+		}),
+	],
 	build: {
 		lib: {
 			entry: './src/index.ts',
 			name: 'RebasedDesignSystem',
-			fileName: 'rebased-design-system',
+			fileName: 'index',
 			formats: ['es', 'umd'],
 		},
 		outDir: './build',
@@ -26,6 +31,5 @@ export default defineConfig({
 			},
 		},
 		sourcemap: 'inline',
-		target: 'esnext',
 	},
 });
